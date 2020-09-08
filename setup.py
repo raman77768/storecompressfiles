3a0ebdd31ff72ca0c411929b4ceab11aa267a664
raman77768 <53868914+raman77768@users.noreply.github.com>
2020-09-08 19:01:11 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'storecompressfiles',      
+  packages = ['storecompressfiles'],  
+  version = '0.1',     
+  license='MIT',       
+  description = 'Package can be used to store text files in compressed format i.e. allocating less space in the memory',   
+  author = 'Ramandeep Singh',                   
+  author_email = 'raman77768@gmail.com',      
+  url = 'https://github.com/raman77768/storecompressfiles', 
+  download_url = 'https://github.com/raman77768/storecompressfiles/archive/v_0.1.tar.gz',    
+  keywords = ['compress', 'small size', 'memory'],   
+  install_requires=[            
+          'sys'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
