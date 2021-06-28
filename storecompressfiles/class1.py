5694c27db46685a1ce8b0c30d44f33009a659d92
raman77768 <raman77768@gmail.com>
2021-06-28 19:25:52 +0530

    removed comments

--- storecompressfiles/class1.py
+++ storecompressfiles/class1.py
@@ -4,7 +4,7 @@ def find_divisor(x):
       return i
 
 def encode(s):
-  #old_size=sys.getsizeof(s)
+
   code=""
   for i in s:
     if i=="’" or i=="‘":continue
@@ -13,7 +13,7 @@ def encode(s):
     code+=z+bin(ord(i)).replace("0b","")
   
   s1,s2="",""
-  #print(len(code))
+
   for i in range(len(code)//2):
     s1+=code[i]
     s2+=code[len(code)-i-1]
@@ -27,15 +27,11 @@ def encode(s):
   if len(code)%2!=0:
     if code[(len(code)//2)]=="0":half_code+="3"
     else:half_code+="4"
-  #new_size=sys.getsizeof(int(half_code))
-  #compress_perc=round(((old_size-new_size)/old_size)*100,2)
-  #print("COMPRESSION RATE: ",compress_perc)
-  #print(half_code)
-  #print(code)
+
   div = find_divisor(int(half_code))
   new_code = int(str(int(half_code)//div)+str(div))
   return new_code
-  #return int(half_code)
+
 
 def decode(encoded_num):
  
@@ -56,7 +52,7 @@ def decode(encoded_num):
     elif i=="0":
       s1+="0"
       s2+="0"
-  #print(s1+s2[::-1])
+
   if str(x)[-1]=="4" or str(x)[-1]=="3":
     if str(x)[-1]=="3":de=s1+"0"+s2[::-1]
     else:de=s1+"1"+s2[::-1]
