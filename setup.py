723ef40594ca74cf6b9860ef9efb69345f2b2993
raman77768 <raman77768@gmail.com>
2021-06-28 19:37:17 +0530

    updated version 0.4

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
+from storecompressfiles.class1 import encode
 setup(
   name = 'storecompressfiles',      
   packages = ['storecompressfiles'],  
-  version = '0.3',     
+  version = '0.4',     
   license='MIT',       
   description = 'Package can be used to store text files in compressed format i.e. allocating less space in the memory',   
   author = 'Ramandeep Singh',                   
   author_email = 'raman77768@gmail.com',      
   url = 'https://github.com/raman77768/storecompressfiles', 
-  download_url = 'https://github.com/raman77768/storecompressfiles/archive/v_0.3.tar.gz',    
-  keywords = ['compress', 'small size', 'memory'],   
-  install_requires=[],
+  download_url = 'https://github.com/raman77768/storecompressfiles/archive/v_0.4.tar.gz',    
+  keywords = ['compress', 'small size', 'memory', 'encode', 'encryption'],   
+  install_requires=['pandas'],
   classifiers=[
     'Development Status :: 3 - Alpha',      
     'Intended Audience :: Developers',      
